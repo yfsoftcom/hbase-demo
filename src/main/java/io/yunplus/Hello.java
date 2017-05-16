dbda30418a9f3ef12e6d2a53783769a3e134e90f
yfsoftcom <yfsoftcom@126.com>
2017-05-16 22:29:25 +0800

    test hbase

--- src/main/java/io/yunplus/Hello.java
+++ src/main/java/io/yunplus/Hello.java
@@ -4,4 +4,8 @@ public class Hello {
     public String sayHello(String name){
         return "Hello "+name+"!";
     }
+
+    public static void main(String[] args){
+        System.out.println(new Hello().sayHello("hbase"));
+    }
 }
